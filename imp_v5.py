2c6d9e49c483d244b761eaa7aa5a7e8bf776d135
AlexisDclqt <alex.dclqt@gmail.com>
2025-08-07 11:20:22 +0200

    imp_v5.py

--- imp_v5.py
+++ imp_v5.py
@@ -10,9 +10,6 @@ import streamlit as st
 
 # df = pd.read_sql(query, conn)
 
-
-st.set_page_config(layout="wide")
-
 def check_password():
     def password_entered():
         if st.session_state["password"] == "deret2025!":
@@ -30,30 +27,28 @@ def check_password():
         return False
     else:
         return True
-
-# 2. Vérification au lancement
+        
 if check_password():
-
-page_1 = st.Page("imp_page_1.py", title= "1. 🤖Implantation Intelligente")
-page_2 = st.Page("imp_page_2.py", title= "2. 📦📂Gestion des Produits")
-page_3 = st.Page("imp_page_3.py", title= "3. 📊 Répartiton des Marques")
-page_4 = st.Page("imp_page_4.py", title= "4. 📊 Répartition des Classe de Rotations")
-page_5 = st.Page("imp_page_5.py", title= "5. 📊 Graphiques Divers")
-
-
-pg = st.navigation([page_1, page_2, page_3, page_4, page_5])
-
-def preserve_session_keys(*keys):
-    for key in keys:
-        if key in st.session_state:
-            st.session_state[key] = st.session_state[key]
-
-preserve_session_keys("LM","axe_imp","axes_res","code_res","zone_res","allee_res","axes_sdv","code_sdv","zone_sdv","allee_sdv",
+    st.set_page_config(layout="wide")
+    
+    page_1 = st.Page("imp_page_1.py", title= "1. 🤖Implantation Intelligente")
+    page_2 = st.Page("imp_page_2.py", title= "2. 📦📂Gestion des Produits")
+    page_3 = st.Page("imp_page_3.py", title= "3. 📊 Répartiton des Marques")
+    page_4 = st.Page("imp_page_4.py", title= "4. 📊 Répartition des Classe de Rotations")
+    page_5 = st.Page("imp_page_5.py", title= "5. 📊 Graphiques Divers")
+    pg = st.navigation([page_1, page_2, page_3, page_4, page_5])
+    
+    def preserve_session_keys(*keys):
+        for key in keys:
+            if key in st.session_state:
+                st.session_state[key] = st.session_state[key]
+                
+                preserve_session_keys("LM","axe_imp","axes_res","code_res","zone_res","allee_res","axes_sdv","code_sdv","zone_sdv","allee_sdv",
                       "marque_sdv","codblo","zone_sdv1","allee_sdv1","zoneMarque",
                       "allMarque","zoneABC","allABC","filtre_abc","CB1","CB2","CB3","CB4","CB5")
+                
+                pg.run()
 
-
-pg.run()
 
 
 
