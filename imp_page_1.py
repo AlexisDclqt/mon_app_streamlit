9f249a627eae689cd10591eaee69b1f45be47259
AlexisDclqt <alex.dclqt@gmail.com>
2025-08-08 12:08:31 +0200

    imp_page_1.py

--- imp_page_1.py
+++ imp_page_1.py
@@ -124,3 +124,7 @@ st.markdown("-------------------------------------------------------------------
 
 
 
+nb_emp_fp = df_FP["Nombre d'emplacements FP"].sum()
+
+st.sidebar.metric(label="Nombre d'emplacements FP dans le dépot :", value= f'{nb_emp_fp} Emplacements FP')
+
