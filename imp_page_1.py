94cc8b1d062b1e1656226b9a0adad77d483f1cb0
AlexisDclqt <alex.dclqt@gmail.com>
2025-08-11 12:38:05 +0200

    imp_page_1.py

--- imp_page_1.py
+++ imp_page_1.py
@@ -22,6 +22,9 @@ def load_data():
     data['Classement Marque A-Z'] = data['Classement Marque A-Z'].astype(str).str.strip().str.upper()
     data[['ABC_ROT','TYPE']] =  data[['ABC_ROT','TYPE']].replace(' ','NAN')
     data['ABC_ROT'] = data['ABC_ROT'].astype(str).str.upper()
+    data['UVC_STOCK'] = pd.to_numeric(data['UVC_STOCK'], errors = 'coerce').astype('Int64')
+    data['UVC_ENCOURS'] = pd.to_numeric(data['UVC_ENCOURS'], errors = 'coerce').astype('Int64')
+    data['UVC_RESTANT'] = pd.to_numeric(data['UVC_RESTANT'], errors = 'coerce').astype('Int64')
 
     return data
 
@@ -127,6 +130,7 @@ st.markdown("-------------------------------------------------------------------
 nb_emp_fp = df_FP["Nombre d'emplacements FP"].sum()
 
 st.sidebar.metric(label="Nombre d'emplacements FP dans le dépot :", value= f'{nb_emp_fp}', border = True)
+
 
 
 
