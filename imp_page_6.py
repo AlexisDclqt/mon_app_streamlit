9b40cee14543a0178ae60fa0670c3703134b3bd7
AlexisDclqt <alex.dclqt@gmail.com>
2025-08-12 16:29:38 +0200

    imp_page_6.py

--- imp_page_6.py
+++ imp_page_6.py
@@ -87,27 +87,27 @@ if axes_selectionnes3:
     df_prep = df_prep[df_prep['AXE_PRODUIT'].isin(axes_selectionnes3)]
     
 
-def color(ligne):
-    if ligne['UVC_RESTANT'] == 0:
-        return ["background-color: red; color: white; font-weight: bold"] * len(ligne)
-    elif 1 <= ligne['UVC_RESTANT'] <= 150:
-        return ["background-color: orange; color: white; font-weight: bold"] * len(ligne)
-    elif 151 <= ligne['UVC_RESTANT'] <= 500:
-        return ["background-color: salmon; color: white; font-weight: bold"] * len(ligne)
-    else:
-        return [""] * len(ligne)
+# def color(ligne):
+#     if ligne['UVC_RESTANT'] == 0:
+#         return ["background-color: red; color: white; font-weight: bold"] * len(ligne)
+#     elif 1 <= ligne['UVC_RESTANT'] <= 150:
+#         return ["background-color: orange; color: white; font-weight: bold"] * len(ligne)
+#     elif 151 <= ligne['UVC_RESTANT'] <= 500:
+#         return ["background-color: salmon; color: white; font-weight: bold"] * len(ligne)
+#     else:
+#         return [""] * len(ligne)
     
     
-df_prep = df_prep.reset_index(drop=True)
+# df_prep = df_prep.reset_index(drop=True)
 
-prep_style = df_prep.style.apply(color, axis=1)
+# prep_style = df_prep.style.apply(color, axis=1)
 
-st.write(prep_style)
-
+st.write(df_prep)
 
 
 
 
  
+
 
 
