c6ee88e020c6cd06095e46d7c01f9198dcfdaa1a
AlexisDclqt <alex.dclqt@gmail.com>
2025-09-05 13:53:18 +0200

    imp_page_6.py

--- imp_page_6.py
+++ imp_page_6.py
@@ -81,7 +81,7 @@ df_abc = df_abc[['CODPRO','ABC_ROT','AXE_PRODUIT']]
 df_abc = df_abc.groupby(['AXE_PRODUIT','ABC_ROT'])['CODPRO'].nunique().reset_index(name = "Nombre d'article")
 
 
-df_prep = data[['CODPRO','AXE_PRODUIT','ZONSTS','ALLSTS','DPLSTS','NIVSTS','UVC_STOCK','UVC_ENCOURS','UVC_RESTANT']]
+df_prep = data[['CODPRO',"ANAPRO","Désignation",'AXE_PRODUIT','ZONSTS','ALLSTS','DPLSTS','NIVSTS','UVC_STOCK','UVC_ENCOURS','UVC_RESTANT']]
 df_prep = df_prep[df_prep['AXE_PRODUIT'] != ""]
 df_prep = df_prep[df_prep['UVC_ENCOURS'] !=0].sort_values(by = 'UVC_ENCOURS',ascending = False)
 
@@ -106,7 +106,7 @@ uvc = data[data['TYPE'] !='Réserve']
 uvc = uvc['UVC_STOCK'].sum()
 uvc_en = data[data['TYPE'] !='Réserve']
 uvc_en = uvc_en['UVC_ENCOURS'].sum()
-percent_uvc = round((uvc_en/uvc)*100,2)
+percent_uvc = ((uvc_en/uvc)*100).round(2)
 
 
 
@@ -131,6 +131,7 @@ st.title("Encours de préparation prévus :")
 axes_selectionnes3 = st.session_state.get("axes_prep",[])
 code_prep = st.session_state.get("code_prep",[])
 nb_enc = st.session_state.get("nb_uvc","")
+choix_marque = st.session_state.get("anapro", [])
 
 axes_selectionnes3 = st.multiselect("Choisir un ou plusieurs axes de préparation", 
                                     options=df_prep['AXE_PRODUIT'].unique(), 
@@ -140,7 +141,7 @@ code_prep = st.text_input("Renseigner un Code Shop: ",
                           key = "code_prep")
 
 nb_enc = st.slider("Nombre d'UVC en ENCOURS",
-                   min_value= int(df_prep['UVC_ENCOURS'].min()),
+                   min_value= 1,
                    max_value= int(df_prep['UVC_ENCOURS'].max()),
                    value=(int(df_prep['UVC_ENCOURS'].min()), int(df_prep['UVC_ENCOURS'].max())),
                    step= 50,
@@ -156,5 +157,65 @@ if nb_enc:
     
 
 
-st.write(df_prep)
+st.dataframe(df_prep, use_container_width= True, height = 600, hide_index= True)
+
+
+st.markdown("----------------------------------------------------------------------------------------------------------")
+st.subheader("Mettre un Titre...")
+
+df_edit = data.copy()
+df_edit = df_edit[(df_edit['TYPE'] != "Réserve") & (df_edit['AXE_PRODUIT'] != "")]
+
+edit_data = df_edit[['CODPRO','ANAPRO',"Désignation",'UVC_STOCK','AXE_PRODUIT']].dropna().sort_values(by = "ANAPRO", ascending = True)
+
+edit_data_code = (edit_data.groupby(["ANAPRO","AXE_PRODUIT"])["CODPRO"].nunique()
+             .reset_index(name = "Nombre de CODPRO par Marque")
+             .sort_values(by = "Nombre de CODPRO par Marque", ascending = False))
+
+edit_data_uvc = (edit_data.groupby(["ANAPRO","AXE_PRODUIT"])["UVC_STOCK"].sum()
+             .reset_index(name = "Nombre d'UVC par Marque")
+             .sort_values(by = "Nombre d'UVC par Marque", ascending = False))
+
+# edit_data_axe = (edit_data.groupby(["AXE_PRODUIT",'ANAPRO'])["UVC_STOCK"].sum()
+#              .reset_index(name = "Nombre d'UVC par Axe")
+#              .sort_values(by = "Nombre d'UVC par Axe", ascending = False))
+
+edit_data_clean = edit_data_code.merge(edit_data_uvc, on = ['ANAPRO',"AXE_PRODUIT"], how = 'inner')
+
+edit_data_pivot = edit_data_clean.pivot(columns = "AXE_PRODUIT", 
+                                        index = ["ANAPRO","Nombre de CODPRO par Marque"], 
+                                        values = "Nombre d'UVC par Marque")
+
+edit_data_pivot = edit_data_pivot.astype("object")  
+edit_data_pivot.fillna("-", inplace=True)
+
+
+choix_marque = st.multiselect("Choisir une Marque: ", options=edit_data_pivot.index.get_level_values("ANAPRO").unique(), key='anapro')
+# choix_axe = st.multiselect("Choisir un Axe: ", options = final_edit.columns.get_level_values('AXE_PRODUIT'), key = "axep")
+
+if choix_marque:
+    edit_data_pivot = edit_data_pivot.loc[edit_data_pivot.index.get_level_values("ANAPRO").isin(choix_marque)]
+
+# if choix_axe:
+#     final_edit = final_edit[final_edit.columns.get_level_values('AXE_PRODUIT').isin(choix_axe)]
+
+st.dataframe(edit_data_pivot, use_container_width= True, height = 600, width = 600,)
+
+
+
+st.data_editor(
+    edit_data_clean, 
+               column_config = {
+                               "Nombre d'UVC par Marque" : 
+                                st.column_config.ProgressColumn("Nb UVC",
+                                format = "%f",
+                                min_value = int(edit_data_clean["Nombre d'UVC par Marque"].min()),
+                                max_value = int(edit_data_clean["Nombre d'UVC par Marque"].max()),
+                                ),
+                                },
+                                hide_index = True,
+                                use_container_width = True,
+                                height = 600)
+
+
 
