67d2b82bc5603abce7a293bd0143267fdd9d679c
AlexisDclqt <alex.dclqt@gmail.com>
2026-02-11 11:53:21 +0100

    Update imp_v5.py

--- imp_v5.py
+++ imp_v5.py
@@ -11,45 +11,45 @@ import streamlit as st
 # df = pd.read_sql(query, conn)
 
 
-def check_password():
-     def password_entered():
-         if st.session_state["password"] == "deret2025!":
-                 st.session_state["password_correct"] = True
-         else:
-             st.session_state["password_correct"] = False
+# def check_password():
+#      def password_entered():
+#          if st.session_state["password"] == "deret2025!":
+#                  st.session_state["password_correct"] = True
+#          else:
+#              st.session_state["password_correct"] = False
 
-     if "password_correct" not in st.session_state:
-         st.text_input("Mot de passe", type="password", on_change=password_entered, key="password")
-         return False
+#      if "password_correct" not in st.session_state:
+#          st.text_input("Mot de passe", type="password", on_change=password_entered, key="password")
+#          return False
 
-     elif not st.session_state["password_correct"]:
-         st.text_input("Mot de passe", type="password", on_change=password_entered, key="password")
-         st.error("Mot de passe incorrect")
-         return False
-     else:
-         return True
+#      elif not st.session_state["password_correct"]:
+#          st.text_input("Mot de passe", type="password", on_change=password_entered, key="password")
+#          st.error("Mot de passe incorrect")
+#          return False
+#      else:
+#          return True
         
-if check_password():
-        st.set_page_config(layout="wide")
+# if check_password():
+st.set_page_config(layout="wide")
         
-        page_1 = st.Page("imp_page_1.py", title= "1. 🤖Implantation Intelligente")
-        page_2 = st.Page("imp_page_2.py", title= "2. 📦📂Gestion des Produits")
-        page_3 = st.Page("imp_page_3.py", title= "3. 📊 Répartiton des Marques")
-        page_4 = st.Page("imp_page_4.py", title= "4. 📊 Répartition des Classe de Rotations")
-        page_5 = st.Page("imp_page_5.py", title= "5. 📊 Graphiques Divers")
-        page_6 = st.Page("imp_page_6.py", title= "6. Gestion des Encours")
+page_1 = st.Page("imp_page_1.py", title= "1. 🤖Implantation Intelligente")
+page_2 = st.Page("imp_page_2.py", title= "2. 📦📂Gestion des Produits")
+page_3 = st.Page("imp_page_3.py", title= "3. 📊 Répartiton des Marques")
+page_4 = st.Page("imp_page_4.py", title= "4. 📊 Répartition des Classe de Rotations")
+page_5 = st.Page("imp_page_5.py", title= "5. 📊 Graphiques Divers")
+page_6 = st.Page("imp_page_6.py", title= "6. Gestion des Encours")
         
-        pg = st.navigation([page_1, page_2, page_3, page_4, page_5,page_6])
+pg = st.navigation([page_1, page_2, page_3, page_4, page_5,page_6])
         
-        def preserve_session_keys(*keys):
-                for key in keys:
-                        if key in st.session_state:
-                                st.session_state[key] = st.session_state[key]
-                                
+def preserve_session_keys(*keys):
+     for key in keys:
+          if key in st.session_state:
+               st.session_state[key] = st.session_state[key]
                                 preserve_session_keys("LM","axe_imp","axes_res","code_res","zone_res","allee_res","axes_sdv","code_sdv","zone_sdv","allee_sdv",
                                                       "marque_sdv","codblo","zone_sdv1","allee_sdv1","zoneMarque",
                                                          "allMarque","zoneABC","allABC","filtre_abc","CB1","CB2","CB3","CB4","CB5","axes_prep","code_prep","nb_uvc","anapro")
-        pg.run()
+pg.run()
+
 
 
 
